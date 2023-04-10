845ac19ab4895db1d8a7f3d2fe8115bf849d5aef
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-04-10 04:44:52 +0300

    функции создающие массив и печатующие его

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше, либо равна
+//3 символам. Первоначальный массив можно ввести с клавиатуры, либо
+//задать на старте выполнения алгоритма. При решении не 
+//пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+void PrintArray(string[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+        System.Console.Write($"{array[i]} ");
+
+    System.Console.WriteLine();
+}
+
+string[] FillArray()
+{
+    string[] userInputArray = new string[99];
+
+    for (int i = 0; i < userInputArray.Length; i++)
+    {
+        string input = Console.ReadLine()!;
+        if (input == "") return userInputArray;
+        else userInputArray[i] = input;
+    }
+    return userInputArray;
+}
+
+string[] array = FillArray();
+PrintArray(array);
